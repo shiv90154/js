7d7968bb2d0ada774cb6f099a25e9f8a4dc54a4e
shiv_kumar <shiva90154@gmail.com>
2024-08-25 16:18:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// x = 12;
+
+// console.log("This is a regular log message.");
+// console.log("The value of x is:", x);
+
+// console.warn("this is warn message")
+// console.error("this is a error message")
+// console.error("this is a error message")
+// name = "abhi"
+// if (name == "shiv") {
+//     console.log('hello')
+// }
+// else if (name == "rohit") {
+//     console.log("hello rohit")
+
+// }
+// else if (name == "abhi") {
+//     console.log(" heloo abhi")
+// }
+// else {
+//     console.log("hello nikhl bhai")
+// // }
+// for (var i = 0; i < 6; i++) {
+//     console.log(i)
+// }
+
+
+// var a = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8, 8, 9, 9,];
+// a.forEach(function (val) {
+//     console.log(val + 12)
+// })
+// console.log(a)
+
+
+// const numbers = [1, 2, 3];
+// numbers.forEach((num, index) => {
+//     console.log(num);
+//     if (num === 2) {
+//         numbers.push(4); // Adds an element to the array
+//     }
+// });
+// // Output: 1, 2, 3, 4
+
+
+// function myFunc(theObject) {
+//     theObject.make = "Toyota";
+//     return
+
+
+// }
+
+// const mycar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+// };
+
+// console.log(mycar.make)
+
+// myFunc(mycar)
+// console.log(mycar.make)
+
+
+function myfunc(theArr) {
+    theArr[0] = 30;
+
+}
+let arr = [45, 23]
+
+console.log(arr[1]);
+console.log(arr)
+myfunc(arr)
+console.log(arr[1]);
+console.log(arr)
+
